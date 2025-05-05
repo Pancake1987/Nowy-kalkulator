13873d1143770e50141863d8f442460c066c6c2d
Pancake1987 <255424@edu.p.lodz.pl>
2025-05-05 14:19:20 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -20,19 +20,20 @@ int main(int argc, char *argv[]) {
         result = numerpierwszy + numerdrugi;
         printf("%d + %d = %d", numerpierwszy, numerdrugi, result);
     }
-    if (strcmp(argv[2], "substract") == 0) {
+    else if (strcmp(argv[2], "substract") == 0) {
         result = numerpierwszy - numerdrugi;
         printf("%d - %d = %d", numerpierwszy, numerdrugi, result);
     }
-    if (strcmp(argv[2], "multiply") == 0) {
+    else if (strcmp(argv[2], "multiply") == 0) {
         result = numerpierwszy * numerdrugi;
         printf("%d * %d = %d", numerpierwszy, numerdrugi, result);
     }
-    if (strcmp(argv[2], "divide") == 0) {
-        result = numerpierwszy / numerdrugi;
-        printf("%d / %d = %d", numerpierwszy, numerdrugi, result);
-        if ( numerdrugi == 0){
-            printf("Nie można dzielić przez 0");
+    else if (strcmp(argv[2], "divide") == 0) {
+        if (numerdrugi == 0){
+            printf("Nie mozna dzielic przez 0");
+        }else {
+            result = numerpierwszy / numerdrugi;
+            printf("%d / %d = %d", numerpierwszy, numerdrugi, result);
         }
     }
     else {
